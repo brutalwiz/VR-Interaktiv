f97fdfdbe0331a79a73e7cdbb9b7c2fd032baf70
brutalwiz <janlukasfeldmann@gmail.com>
2019-05-12 19:16:34 +0200

    Updated World Map/GitIgnore
    Updated World map with floating Balls and added Blender folder to project and to gitignore

---
+++ Assets/Scripts/RandomSpawnMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnMovement : MonoBehaviour
+{
+    public float minThrust;
+    public float maxThrust;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.Rotate((int)Random.Range(0f,360f), (int)Random.Range(0f, 360f), (int)Random.Range(0f, 360f));
+        rb.freezeRotation = true;
+        Impulse(transform.forward*Random.Range(minThrust,maxThrust));
+    }
+    private Vector3 oldVelocity;
+    private void FixedUpdate()
+    {
+        oldVelocity = rb.velocity;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Vector3 refVelocity = Vector3.Reflect(oldVelocity, contact.normal);
+        rb.velocity = refVelocity;
+
+        Quaternion rotation = Quaternion.FromToRotation(oldVelocity, refVelocity);
+        transform.rotation = rotation * transform.rotation;
+    }
+    void Impulse(Vector3 dir)
+    {
+        rb.AddForce(dir, ForceMode.Impulse);
+    }
+}
