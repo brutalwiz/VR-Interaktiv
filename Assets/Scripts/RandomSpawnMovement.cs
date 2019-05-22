c69d622d3c1394a44b9003537c0fe0fa850d6c32
Jan Feldmann <janlukasfeldmann@gmail.com>
2019-05-22 12:05:13 +0200

    Added Sinus Movement to Balls

--- Assets/Scripts/RandomSpawnMovement.cs
+++ Assets/Scripts/RandomSpawnMovement.cs
@@ -6,14 +6,20 @@ public class RandomSpawnMovement : MonoBehaviour
 {
     public float minThrust;
     public float maxThrust;
+
+    public float freq = 0.5f;
+    public float magnitude = 0.5f;
     private Rigidbody rb;
+    int phase;
     // Start is called before the first frame update
     void Start()
     {
+        freq = freq*Random.Range(0.5f,1.5f);
+        phase = Random.Range(0,360);
         rb = GetComponent<Rigidbody>();
-        transform.Rotate((int)Random.Range(0f,360f), (int)Random.Range(0f, 360f), (int)Random.Range(0f, 360f));
+        //transform.Rotate((int)Random.Range(0f,360f), (int)Random.Range(0f, 360f), (int)Random.Range(0f, 360f));
         rb.freezeRotation = true;
-        Impulse(transform.forward*Random.Range(minThrust,maxThrust));
+        //Impulse(transform.forward*Random.Range(minThrust,maxThrust));
     }
     private Vector3 oldVelocity;
     private void FixedUpdate()
@@ -23,7 +29,8 @@ public class RandomSpawnMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Sinus Movement
+        transform.position = transform.position+transform.up *Mathf.Sin(Time.time * freq+phase) * magnitude;
     }
     private void OnCollisionEnter(Collision collision)
     {
